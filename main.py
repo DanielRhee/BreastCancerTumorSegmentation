8323db322d270e1e92736ae39873453e8d2fbe3e
ecbostrom <bostromec@gmail.com>
2025-02-23 17:36:07 -0800

    save button

--- main.py
+++ main.py
@@ -3,26 +3,36 @@ from tkinter import ttk
 from tkinter import filedialog
 from PIL import ImageTk, Image
 from Models import predict
+import os
 
+root = Tk()
 model = predict.predictor()
+pil_images = []
 
-def save(result_image):
-    file = filedialog.asksaveasfile(defaultextension=".png", 
-                                     filetypes=[("png", "*.png")
-                                     ])
-    result_image.save(file)
+def save():
+    for pillow_image in pil_images:
+        file = filedialog.asksaveasfile(
+            mode="w",
+            title="Save File As",
+            initialdir="~",
+            initialfile="cancer_diagnosis.png",
+            defaultextension=".png",
+            filetypes=[("PNG file", "*.png"), ("All files", "*.*")]
+        )
+        pillow_image.save(file.name)
 
 def openImage():
     #entry_frame is a row with the png and associated buttons
     image_path = filedialog.askopenfilename(initialdir="~", title="upload ultra sound image", filetypes=(("png", "*.png"), ("all files", "*,*")))
     pillow_image = Image.open(image_path)
     result_image, classification_num = model.predict(pillow_image.convert("RGB"))
+    pil_images.append(result_image)
 
     #this turns the image into a widget so it can be displayed
     classification_key = {0:"beign", 1: "malignant", 2:"normal"}
     classification = classification_key[classification_num]
     result_tkimage = ImageTk.PhotoImage(result_image)
-    result_label = Label(scrollable_frame, image=result_tkimage, text=classification, compound=TOP)
+    result_label = Label(scrollable_frame, image=result_tkimage, text=classification, font=("Arial", 30), compound=TOP)
     result_label.image = result_tkimage
     #save_btn = Button(entry_frame, test= "save", command=save)
     #TODO add a delete button. this will likely include tracking and deleting the frame
@@ -37,10 +47,10 @@ def main():
     root.mainloop()
 
 if __name__ == "__main__":
-    root = Tk()
+    
     root.title("Breast Cancer Tumor Segmentation")
     upload_btn = Button(root, text= "upload file", command= openImage).pack()
-    save_btn = Button(root, text= "Save Results")
+    save_btn = Button(root, text= "Save Results", command= save).pack()
     canvas = Canvas(root)
     canvas.pack(side=LEFT, fill=BOTH, expand=True)
 
